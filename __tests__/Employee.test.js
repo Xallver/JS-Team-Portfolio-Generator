1058326d67b1c152db68dae84391bae15e932902
Xallver <jamesstanley11@gmail.com>
2022-02-18 20:13:54 -0500

    went back thru project instructions and re-did test and employee to match needed classes and properties/methods for such class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,11 @@
 const Employee = require('../lib/Employee');
 
 test('creates the employee with info', () => {
-    const employee = new Employee('Eli', 1, "eli@gmail.com", "Elighub");
+    const employee = new Employee('Eli', 1, "eli@gmail.com", "Employee");
 
-    expect(employee.name).toBe('Eli');
-    expect(employee.id).toBe(1);
-    expect(employee.email).toBe("eli@gmail.com");
-    expect(employee.github).toBe("Elighub");
+    expect(employee.getName()).toBe("Eli");
+    expect(employee.getId()).toBe(1);
+    expect(employee.getEmail()).toBe("eli@gmail.com");
+    expect(employee.getRole()).toBe("Employee");
 });
+
